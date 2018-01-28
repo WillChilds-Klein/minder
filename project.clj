e78fd51305943d273c73f91a6023e96710f917ea
WillChilds-Klein <willck93@gmail.com>
2018-01-27 22:47:14 -0500

    same thing, but now working with real JSON

--- project.clj
+++ project.clj
@@ -2,9 +2,6 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/data.json "0.2.6"]
                  [clj-time "0.14.2"]
-                 [ring/ring-core "1.6.3"]
-                 [ring/ring-jetty-adapter "1.6.3"]
-                 [com.amazonaws/aws-lambda-java-core "1.1.0"]]
-  :java-source-paths ["src/java"]
+                 [uswitch/lambada "0.1.2"]]
   :aot
   :all)
--- src/minder/server.clj
+++ src/minder/server.clj
@@ -1,12 +1,18 @@
 (ns minder.server
- (:require
-    [clojure.data.json :as json])
-  (:gen-class
-    :name minder.server.Handlers
-    :implements [com.amazonaws.services.lambda.runtime.RequestStreamHandler]
-    :methods [[lambdaPostHandler [models.IfttTweet] String]]))
+  (:require
+    [clojure.data.json :as json]
+    [clojure.java.io :as io]
+    [uswitch.lambada.core :refer [deflambdafn]]))
 
-(defn -lambdaPostHandler
-  [this tweet]
-  (->> {:bodytext (str "here's the content of the tweet: " (.getText tweet))}
-       (json/write-str)))
+(defn handler
+  [event]
+  {:bodytext (str "here's the content of the tweet: " (:text event))})
+
+(deflambdafn
+  minder.server.lambdaPostHandler
+  [in out ctx]
+  (let [event (json/read (io/reader in) :key-fn keyword)
+        res (handler event)]
+    (with-open [w (io/writer out)]
+      (json/write res w))))
+
